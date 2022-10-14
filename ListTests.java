b0d225d83e2ec03e3784a0a5adee4d5506c3e77c
arnavkamra14 <arnavkamra14@gmail.com>
2022-10-14 02:44:08 -0700

    Create ListTests.java

---
+++ ListTests.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListTests {
+    @Test
+    public void filterTest() {
+        List<String> input1 = new ArrayList<>();
+        input1.add("Hello");
+        input1.add("My");
+        input1.add("Name");
+        List<String> expected = new ArrayList<>();
+        expected.add("Hello");
+
+        assertArrayEquals(expected.toArray(), ListExamples.filter(input1, new IsHello()).toArray());
+
+    }
+
+    @Test
+    public void mergeTest() {
+
+    }
+
+}
