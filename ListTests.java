58d51ad671da6e5477468cbb850787f1a2ab8c01
arnavkamra14 <arnavkamra14@gmail.com>
2022-10-28 22:56:25 -0700

    Update ListTests.java

--- ListTests.java
+++ ListTests.java
@@ -5,15 +5,27 @@ import java.util.List;
 
 public class ListTests {
     @Test
-    public void filterTest() {
-        List<String> input1 = new ArrayList<>();
-        input1.add("Hello");
-        input1.add("My");
-        input1.add("Name");
-        List<String> expected = new ArrayList<>();
-        expected.add("Hello");
+    public void mergeTest() {
 
-        assertArrayEquals(expected.toArray(), ListExamples.filter(input1, new IsHello()).toArray());
+        List<String> input1 = new ArrayList<>();
+        input1.add("Apple");
+        input1.add("Bat");
+        input1.add("Firefighter");
+
+        List<String> input2 = new ArrayList<>();
+        input2.add("Cat");
+        input2.add("Dog");
+        input2.add("Elephant");
+
+        List<String> expected = new ArrayList<>();
+        expected.add("Apple");
+        expected.add("Bat");
+        expected.add("Cat");
+        expected.add("Dog");
+        expected.add("Elephant");
+        expected.add("Firefighter");
+
+        assertArrayEquals(expected.toArray(), (ListExamples.merge(input1, input2)).toArray());
 
     }
 
